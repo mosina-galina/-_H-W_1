79463106530c80dce2d735b1131fb7ef38f05d5f
Galina <galinka-ver@mail.ru>
2022-12-24 00:41:53 +0500

    Сравнение 2 чисел

---
+++ task1/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.WriteLine("Введите первое чило");
+string numberA = Console.ReadLine();
+int a = int.Parse(numberA);
+Console.WriteLine("Введите второе чило");
+string numberB = Console.ReadLine();
+int b = int.Parse(numberB);
+int max = 0;
+int min = 0;
+if (a>b) 
+{
+    max = a;
+    min = b;
+}
+else
+{
+    max = b;
+    min = a;
+}
+Console.WriteLine($"Большее число {max}, а меньшее {min}");
