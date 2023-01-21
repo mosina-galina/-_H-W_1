a44b0186e314f8d1ac5c1026b82fe6dfc9a7a56b
Galina <galinka-ver@mail.ru>
2023-01-21 21:35:35 +0500

    Разница меджу максимальным и минимальным

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,20 +1,22 @@
-﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-Console.WriteLine("Введите первое чило");
-string numberA = Console.ReadLine();
-int a = int.Parse(numberA);
-Console.WriteLine("Введите второе чило");
-string numberB = Console.ReadLine();
-int b = int.Parse(numberB);
-int max = 0;
-int min = 0;
-if (a>b) 
-{
-    max = a;
-    min = b;
+﻿//Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+Console.WriteLine("Введите длинну массива ");
+int n = int.Parse(Console.ReadLine() ?? "");
+double[] array = new double[n];
+for (int i = 0; i < n; i++)
+{  
+    array[i] = new Random().Next(1,1000);
+    Console.Write($"{array[i]} ");
 }
-else
+double max = array[0];
+double min = array[0];
+for (int k = 0; k < n; k++)
 {
-    max = b;
-    min = a;
+        if (array[k] > max) max = array[k];
+        else
+        {
+            if (array[k] < min) min = array [k];
+        }
 }
-Console.WriteLine($"Большее число {max}, а меньшее {min}");
+Console.WriteLine($" максимальное - {max}, минимальное - {min}");
+double p= max-min;
+Console.WriteLine($"разница между максимальным и минимальным = {p}");
